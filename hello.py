0e4a80dc34c2cadfe48e7bc32a77807645a9080b
theafreen <81609646+theafreen@users.noreply.github.com>
2024-04-05 23:59:29 +0530

    added input odd fn.py

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("hello world")
+#Create a list of all odd numbers between 1 and a max number. Max number is something you need to take from a user using input() function
+max = int(input("enter max num"))
+odd_num=[]
+for i in range(i,max)
+  if i%2==1:
+    odd_num.append(i)
+print(odd_num)
