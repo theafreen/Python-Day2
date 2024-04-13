63590afc0294e8193853455146726c51e359b64c
theafreen <81609646+theafreen@users.noreply.github.com>
2024-04-13 21:40:39 +0530

    leetcode

--- hello.py
+++ hello.py
@@ -5,3 +5,14 @@ for i in range(1,max):
   if i%2==1:
     odd_num.append(i)
 print(odd_num)
+
+#find-words-containing-character/ leetcode 
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        store=[]
+        low=0
+        for word in words:
+            if x in word:
+                store.append(low)
+            low+=1
+        return store
